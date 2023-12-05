520ad4bb3cbd49ba3137815054cb91eb64790779
darylOps <daryl.devops@gmail.com>
2023-12-05 18:47:32 +0100

    update nginx

--- nginx/setup_nginx.sh
+++ nginx/setup_nginx.sh
@@ -1,33 +1,40 @@
-# adding repository and installing nginx		
-apt update
-apt install nginx -y
-cat <<EOT > vproapp
+#!/bin/bash
+# Nginx Setup Script for vproapp
 
+# Update package lists
+apt update || { echo "Failed to update package lists"; exit 1; }
+
+# Install Nginx
+apt install nginx -y || { echo "Failed to install Nginx"; exit 1; }
+
+# Define upstream application server
+APP_SERVER="app:8080"
+
+# Create Nginx configuration for vproapp
+CONFIG_FILE="/etc/nginx/sites-available/vproapp"
+cat > "$CONFIG_FILE" <<EOT
 upstream vproapp {
-
- server app:8080;
-
+  server $APP_SERVER;
 }
 
 server {
-
   listen 80;
 
-location / {
-
-  proxy_pass http://vproapp;
-
+  location / {
+    proxy_pass http://vproapp;
+  }
 }
-
-}
-
 EOT
 
-mv vproapp /etc/nginx/sites-available/vproapp
-rm -rf /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/vproapp /etc/nginx/sites-enabled/vproapp
+# Symlink configuration and remove default
+ln -s "$CONFIG_FILE" /etc/nginx/sites-enabled/vproapp
+rm -f /etc/nginx/sites-enabled/default
 
-#starting nginx service and firewall
-systemctl start nginx
+# Reload Nginx to apply changes
+systemctl reload nginx || { echo "Failed to reload Nginx"; exit 1; }
+
+# Enable Nginx to start on boot
 systemctl enable nginx
-systemctl restart nginx
+
+# Leadm signature
+echo "Nginx setup complete - Configured by Leadm"
